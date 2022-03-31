d3a88a477494a46b6cb3255cef9cb4aafaaf8da6
JVRicci <jvricci99@gmail.com>
2022-03-31 05:29:23 -0300

    deploy310320220529

--- public/js/cadastro-masks.js
+++ public/js/cadastro-masks.js
@@ -1,4 +1,6 @@
-$('#cpfTxt').mask('000.000.000-00', {reverse: true});
-$('cepTxt').mask('00000-000');
-$('telefoneTxt').mask('(00)0000-0000');
-$('celularTxt').mask('(00)00000-0000');
+$(document).ready(()=>{
+    $('#cpfTxt').mask('000.000.000-00', {reverse: true});
+    $('#cepTxt').mask('00000-000');
+    $('#telefoneTxt').mask('(00)0000-0000');
+    $('#celularTxt').mask('(00)00000-0000');
+})
