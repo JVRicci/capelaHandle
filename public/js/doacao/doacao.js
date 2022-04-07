633b02a3960c8a3fa8247b716c1be2e55fc5ae4b
JVRicci <jvricci99@gmail.com>
2022-04-07 03:29:41 -0300

    deploy070420220329

--- public/js/doacao/doacao.js
+++ public/js/doacao/doacao.js
@@ -6,13 +6,43 @@ function exibeDoacao(){
 
 function carregaId(elemento){
     var colunaDoador = $('#'+elemento);
-    var linhaDoador = $('#doacao-table').find('tr:eq('+elemento+')').text();
-    alert(linhaDoador);
+    var linhaDoador = $('#doacao-table tr').find('td:eq(0)').text();
+    //var linhaDoador = $('#doacao-table').rows
+    var lista= $('#doacao-table tbody')
+
+
+    let doacaofull = []
+    let doacaoDoador = []
+
+
+    $(lista).find("tr").each((index, tr)=>{
+        doacaofull.push([[$(tr).find('td:eq(0)').html()],
+                        [$(tr).find('td:eq(1)').html()],
+                        [$(tr).find('td:eq(2)').html()],
+                        [$(tr).find('td:eq(3)').html()],
+                        [$(tr).find('td:eq(4)').html()]],)
+        
+       
+        if(doacaofull[index][0] == elemento){
+
+            doacaoDoador.push("<tr><td>"+[$(tr).find('td:eq(0)').html()]+"</td>"+
+            "<td>"+[$(tr).find('td:eq(1)').html()]+"</td>"+
+            "<td>"+[$(tr).find('td:eq(2)').html()]+"</td>"+
+            "<td>"+[$(tr).find('td:eq(3)').html()]+"</td>"+
+            "<td>"+[$(tr).find('td:eq(4)').html()]+"</td></tr>")
+        }
+        
+     });
+
+    $('#doacao-table tbody tr').css( "display","none");
+
+    var doacaoArray = doacaoDoador.filter((este,i)=>doacaoDoador.indexOf(este)===i)
+    $('#doacao-table tbody').append(doacaoArray)
 
     $('#doadorIdTxt').val(elemento)
     $('#doadorSel'+elemento).prop('checked',true);
 
-    $('#doacao-table tbody tr').css( "display","none");
-    colunaDoador.css("display","table-row")
-    
+    //colunaDoador.css("display","table-row")
+
+     
 };
