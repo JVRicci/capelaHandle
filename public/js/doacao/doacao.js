8e1835d94ee7a721f92b1fdb914d64638d275493
JVRicci <jvricci99@gmail.com>
2022-04-07 15:37:37 -0300

    deploy070420221537

--- public/js/doacao/doacao.js
+++ public/js/doacao/doacao.js
@@ -1,19 +1,11 @@
 
-function exibeDoacao(){
-
-};
-
+const listaDoa = $('#doador-table tbody ');
 
 function carregaId(elemento){
-    var colunaDoador = $('#'+elemento);
-    var linhaDoador = $('#doacao-table tr').find('td:eq(0)').text();
-    //var linhaDoador = $('#doacao-table').rows
     var lista= $('#doacao-table tbody')
-
 
     let doacaofull = []
     let doacaoDoador = []
-
 
     $(lista).find("tr").each((index, tr)=>{
         doacaofull.push([[$(tr).find('td:eq(0)').html()],
@@ -31,7 +23,6 @@ function carregaId(elemento){
             "<td>"+[$(tr).find('td:eq(3)').html()]+"</td>"+
             "<td>"+[$(tr).find('td:eq(4)').html()]+"</td></tr>")
         }
-        
      });
 
     $('#doacao-table tbody tr').css( "display","none");
@@ -42,7 +33,50 @@ function carregaId(elemento){
     $('#doadorIdTxt').val(elemento)
     $('#doadorSel'+elemento).prop('checked',true);
 
-    //colunaDoador.css("display","table-row")
-
      
 };
+
+
+
+
+//função de pesquisa
+function pesquisar(elemento){
+
+    var valElemento = elemento
+
+    var nomes = $('#doador-table tbody').text();
+    
+
+    var result= []
+
+    $(listaDoa).find('tr').each((index, tr)=>{
+
+        var nomeAtual = $(tr).find('td:eq(2)').html().toLowerCase()
+        if(nomeAtual.indexOf(elemento) != -1) {
+        
+            result.push('<tr onclick="carregaId('+[$(tr).find('td:eq(1)').html()]
+            +')"><td>'+[$(tr).find('td:eq(0)').html()]+"</td>"+
+            "<td>"+[$(tr).find('td:eq(1)').html()]+"</td>"+
+            "<td>"+[$(tr).find('td:eq(2)').html()]+"</td></tr>")}
+
+
+    })
+
+    
+    if(elemento==""){ 
+        
+        var doadorArray = result.filter((este,i)=>result.indexOf(este)===i)
+        $('#doador-table tbody tr').html(listaDoa);
+        
+
+    }
+    else{   
+    $('#doador-table tbody tr').css( "display","none");
+
+    var doadorArray = result.filter((este,i)=>result.indexOf(este)===i)
+
+    listaDoa.html(doadorArray)
+    }
+
+}
+
