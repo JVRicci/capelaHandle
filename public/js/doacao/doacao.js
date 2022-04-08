a3dfbfba0c1d4e49d951e2032301e82cbc49ddd0
JVRicci <jvricci99@gmail.com>
2022-04-08 02:57:12 -0300

    deploy08042022 campo pesquisa

--- public/js/doacao/doacao.js
+++ public/js/doacao/doacao.js
@@ -1,5 +1,59 @@
-
+//carrega tabela doador
 const listaDoa = $('#doador-table tbody ');
+
+const listaDoador = $('#doador-table tbody ');
+
+var nomes = $('#doador-table tbody').text();
+
+var tabelaFull= []
+
+$(listaDoador).find('tr').each((index, tr)=>{
+
+    tabelaFull.push('<tr onclick="carregaId('+[$(tr).find('td:eq(1)').html()]
+    +')"><td>'+[$(tr).find('td:eq(0)').html()]+"</td>"+
+    "<td>"+[$(tr).find('td:eq(1)').html()]+"</td>"+
+    "<td>"+[$(tr).find('td:eq(2)').html()]+"</td></tr>")
+
+})
+
+//função de pesquisa
+function pesquisar(elemento){
+
+    var valElemento = elemento
+
+    var nomes = $('#doador-table tbody').text();
+    
+    var result= []
+
+    $(listaDoador).find('tr').each((index, tr)=>{
+
+        var nomeAtual = $(tr).find('td:eq(2)').html().toLowerCase()
+        if(nomeAtual.indexOf(elemento) != -1) {
+        
+            result.push('<tr onclick="carregaId('+[$(tr).find('td:eq(1)').html()]
+            +')"><td>'+[$(tr).find('td:eq(0)').html()]+"</td>"+
+            "<td>"+[$(tr).find('td:eq(1)').html()]+"</td>"+
+            "<td>"+[$(tr).find('td:eq(2)').html()]+"</td></tr>")}
+    })
+    
+    if(elemento==""){ 
+        
+        var doadorArray = result.filter((este,i)=>result.indexOf(este)===i)
+        $('#doador-table tbody').html(tabelaFull);
+        
+    }
+    else{   
+    $('#doador-table tbody tr').html("");
+    var doadorArray = result.filter((este,i)=>result.indexOf(este)===i)
+
+    listaDoa.html(doadorArray)
+    }
+}
+
+
+
+
+//carrega doações
 
 function carregaId(elemento){
     var lista= $('#doacao-table tbody')
@@ -35,48 +89,3 @@ function carregaId(elemento){
 
      
 };
-
-
-
-
-//função de pesquisa
-function pesquisar(elemento){
-
-    var valElemento = elemento
-
-    var nomes = $('#doador-table tbody').text();
-    
-
-    var result= []
-
-    $(listaDoa).find('tr').each((index, tr)=>{
-
-        var nomeAtual = $(tr).find('td:eq(2)').html().toLowerCase()
-        if(nomeAtual.indexOf(elemento) != -1) {
-        
-            result.push('<tr onclick="carregaId('+[$(tr).find('td:eq(1)').html()]
-            +')"><td>'+[$(tr).find('td:eq(0)').html()]+"</td>"+
-            "<td>"+[$(tr).find('td:eq(1)').html()]+"</td>"+
-            "<td>"+[$(tr).find('td:eq(2)').html()]+"</td></tr>")}
-
-
-    })
-
-    
-    if(elemento==""){ 
-        
-        var doadorArray = result.filter((este,i)=>result.indexOf(este)===i)
-        $('#doador-table tbody tr').html(listaDoa);
-        
-
-    }
-    else{   
-    $('#doador-table tbody tr').css( "display","none");
-
-    var doadorArray = result.filter((este,i)=>result.indexOf(este)===i)
-
-    listaDoa.html(doadorArray)
-    }
-
-}
-
